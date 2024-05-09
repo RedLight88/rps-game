ec6b86d6ce8a345d8ec3d542d89c64f2b50a3ada
RedLight88 <robert.gm644@gmail.com>
2024-05-09 15:11:23 +0300

    added reset button and round logic

--- logic.js
+++ logic.js
@@ -4,6 +4,53 @@ let pw = 0;
 let cw = 0;
 let td = 0;
 
+let gamesPlayed = 0;
+
+const buttons = document.querySelectorAll('.round');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        const rounds = this.value;
+        bestOf(rounds);
+        buttons.forEach(btn => {
+            btn.disabled = true;  
+        });
+    });
+});
+
+const reset = document.querySelector('button#reset');
+reset.addEventListener('click', function() {
+    location.reload();
+})
+
+function bestOf(rounds) {
+    const playButton = document.getElementById('play');
+    const messageDisplay = document.getElementById('message'); 
+    playButton.addEventListener('click', function() {
+        gamesPlayed++;
+        if (gamesPlayed >= rounds) {
+            let winner = determineWinner(); 
+            messageDisplay.textContent = winner + ' wins the match!'; 
+            playButton.disabled = true; 
+        }
+    });
+}
+
+
+
+function determineWinner() {
+    if (pw > cw) {
+        return 'Player';
+    } else if (cw > pw) {
+        return 'Computer';
+    } else {
+        return 'No clear winner'; 
+    }
+}
+
+
+
+
 function getComputerChoice() {
     
     const i= Math.floor(Math.random() * 3);
@@ -42,10 +89,11 @@ function display(){
     document.getElementById('draw').innerHTML = 'Total draws: ' + td;
 }
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('play').addEventListener('click', display);
     
-    const buttons = document.querySelectorAll('round');
     
 });
 
